ed3ad7852253494b16cd76f310b577846c986a33
inb18 <90775558+inb18@users.noreply.github.com>
2021-11-23 17:51:22 +0530

    Update apply-ocr-to-s3-object.py

--- apply-ocr-to-s3-object.py
+++ apply-ocr-to-s3-object.py
@@ -20,14 +20,14 @@ def apply_ocr_to_document_handler(event, context):
         bucket = event.get('s3', {}).get('bucket', {}).get('name')
         key = urllib.parse.unquote_plus(event.get('s3', {}).get('object', {}).get('key', ''), encoding='utf-8')
         if bucket and key != '':
-            pages = event.get('pages')
+            
             do_backup = event.get('doBackup')
             uuidstr = str(uuid.uuid1())
             try:
                 inputname = '/tmp/input' + uuidstr + '.pdf'
                 outputname = '/tmp/output' + uuidstr + '.pdf'
                 s3.download_file(Bucket=bucket, Key=key, Filename=inputname)
-                ocrmypdf.ocr(inputname, outputname, pages=pages, force_ocr=True, lambda_safe=True)
+                ocrmypdf.ocr(inputname, outputname, force_ocr=True, lambda_safe=True)
                 if do_backup:
                     s3.upload_file(inputname, bucket, key + '.bak')
                 s3.upload_file(outputname, bucket, key)
